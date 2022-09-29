06d0cd27d9c97a1006fb7a5102e859b900eb9eae
unknown <Arundattkaushik>
2022-09-30 01:05:40 +0530

    commit at 01:06 am

--- Desktop/Daily Kumute/Java Practice/hellogit/src/hellogit/Git1.java
+++ Desktop/Daily Kumute/Java Practice/hellogit/src/hellogit/Git1.java
@@ -5,7 +5,6 @@ public class Git1 {
 	public static void main(String[] args) {
 		System.out.println("first change");
 		System.out.println("first change");
-		System.out.println("first change");
 	}
 
 }
