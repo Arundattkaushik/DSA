c2c6a4ee8b347a54e78c26be17a74d23a34cc391
unknown <Arundattkaushik>
2022-09-29 21:50:26 +0530

    updated file

--- Desktop/Daily Kumute/Java Practice/hellogit/src/hellogit/Git1.java
+++ Desktop/Daily Kumute/Java Practice/hellogit/src/hellogit/Git1.java
@@ -3,7 +3,7 @@ package hellogit;
 public class Git1 {
 
 	public static void main(String[] args) {
-
+		System.out.println("This is my first change");
 	}
 
 }
--- eclipse-workspace/Calculator/src/simpleCalculation/SumNumbers.java
+++
@@ -1,9 +0,0 @@
-package simpleCalculation;
-
-public class SumNumbers {
-	
-	public static void main(String[] args) {
-		System.out.println("Sum Numbers");
-	}
-
-}
