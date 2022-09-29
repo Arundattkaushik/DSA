6919dae8eec26c17585a55be074f8b54154d24b6
unknown <Arundattkaushik>
2022-09-30 00:21:40 +0530

    commit on slave

--- Desktop/Daily Kumute/Java Practice/hellogit/src/hellogit/Git1.java
+++ Desktop/Daily Kumute/Java Practice/hellogit/src/hellogit/Git1.java
@@ -5,6 +5,7 @@ public class Git1 {
 	public static void main(String[] args) {
 		System.out.println("first change");
 		System.out.println("first change");
+		System.out.println("first change");
 	}
 
 }
