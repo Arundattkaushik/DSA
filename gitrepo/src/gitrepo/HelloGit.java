90b447e446f5d065c422714c20c138443a478912
unknown <Arundattkaushik>
2022-09-29 19:42:52 +0530

    this is my first commit

---
+++ gitrepo/src/gitrepo/HelloGit.java
@@ -0,0 +1,10 @@
+package gitrepo;
+
+public class HelloGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Git");
+		System.out.println("this is second change");
+	}
+
+}
